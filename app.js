8e59ead9cfa2ae985f2d724a21ac029fcd2a0ea3
fengxinming <fxm0016@126.com>
2018-06-24 06:30:57 +0800

    首次提交

---
+++ app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const http = require('http');
+const os = require('os');
+const fs = require('fs');
+const Path = require('path');
+const conf = require('./conf/server');
+const Router = require('./lib/router');
+const formidable = require('./lib/formidable');
+
+const router = new Router();
+router.use(router.resource());
+router.post('/upload', (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  const contentType = req.headers['content-type'];
+  if (contentType.indexOf('multipart/form-data')) {
+    res.statusCode = 405;
+    res.end(JSON.stringify({
+      message: '不允许上传'
+    }), 'utf8');
+    return;
+  }
+  const form = new formidable.IncomingForm();
+  const files = [];
+  const fields = [];
+
+  const uploadDir = Path.join(__dirname, '/data');
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  form.uploadDir = uploadDir;
+  form
+    .on('field', function(field, value) {
+      fields.push([field, value]);
+    })
+    .on('file', function(field, file) {
+      files.push([field, file]);
+    })
+    .on('end', function() {
+      files.forEach((file) => {
+        file = file[1];
+        const pobj = Path.parse(file.path);
+        fs.renameSync(file.path, Path.join(pobj.dir, file.name));
+      });
+      res.statusCode = 200;
+      res.end(JSON.stringify({
+        message: '上传成功'
+      }), 'utf8');
+    })
+    .parse(req);
+});
+
+const server = http.createServer(router.callback);
+server.listen(conf.port);
+server.on('listening', () => {
+  console.log(`The server is running at port ${conf.port}`);
+});
---
+++ conf/server.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const app = {
+  port: 3000
+};
+
+module.exports = app;
